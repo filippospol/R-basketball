0325d74a893f36f4bc1b05f7a6d7656695a55844
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-01-18 14:26:55 +0200

    Update ggplot-options.R

--- ggplot-options.R
+++ ggplot-options.R
@@ -3,7 +3,7 @@ library(showtext)
 
 font_add_google(name = "Roboto", family = "Roboto")
 showtext_auto()
-theme_set(theme_minimal)
+theme_set(theme_bw)
 theme_update(text=element_text(family = 'Roboto'),
              plot.title = element_text(hjust=0.5, size=28,face="bold"),
              plot.subtitle = element_text(hjust=0.5, size=20),
