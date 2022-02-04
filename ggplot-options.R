ed5d36b61fe7c4a80ac6655ff3cec5d21fb52904
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-02-04 13:10:53 +0000

    Update ggplot-options.R

--- ggplot-options.R
+++ ggplot-options.R
@@ -13,6 +13,6 @@ theme_light() +
 theme(text=element_text(family = "Roboto"),
              plot.title = element_text(hjust=0, size=28,face="bold"),
              plot.subtitle = element_text(hjust=0, size=20, face="italic"),
-             plot.caption = element_text(h_just=0, size=18),
+             plot.caption = element_text(hjust=0, size=18),
              axis.title = element_text(size=18),
              axis.text = element_text(size=15))
