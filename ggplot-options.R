89277dc9e6b3afeff87167920eda60abd1678e70
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-02-07 19:18:38 +0000

    Update ggplot-options.R

--- ggplot-options.R
+++ ggplot-options.R
@@ -11,10 +11,12 @@ font_add_google(name = "Roboto", family = "Roboto")
 showtext_auto()
 
 p +
-theme_light() +
+theme_minimal() +
 theme(text=element_text(family = "Roboto"),
              plot.title = element_text(hjust=0, size=28,face="bold"),
              plot.subtitle = element_text(hjust=0, size=20, face="italic"),
              plot.caption = element_text(hjust=0, size=18),
              axis.title = element_text(size=18),
              axis.text = element_text(size=15))
+
+ggsave("name.png",p,bg="#ffffff")
