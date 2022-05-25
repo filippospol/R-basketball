da2296a4c3ab1ee515a67509c689e110bb3ffa55
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-05-25 14:52:37 +0100

    Update bbref-standings.R

--- basketball-reference/bbref-standings.R
+++ basketball-reference/bbref-standings.R
@@ -1,48 +1,21 @@
 # load libraries ----
-library(tidyverse)
-library(janitor)
-library(rvest)
+pacman::p_load(tidyverse,rvest,janitor)
 
-# get expanded standings & team-vs-team tables: ----
-bbrefStandings <- "https://www.basketball-reference.com/leagues/NBA_2022_standings.html#expanded_standings" %>% 
+# get expanded standings: ----
+exp_standings <- "https://www.basketball-reference.com/leagues/NBA_2022_standings.html#expanded_standings" %>% 
   read_html() %>% 
   html_nodes(xpath = '//comment()') %>%    
   html_text() %>%    
   paste(collapse = '') %>%    
   read_html() %>% 
-  html_table()
-
-bbrefStandings[[1]] <- bbrefStandings[[1]] %>% 
+  html_table() %>% 
+  pluck(1) %>% 
   row_to_names(row_number=1) %>% 
-  select(-1) %>% 
-  rename_all(.funs = toupper) %>%
-  rename(TEAM_NAME=TEAM,VS_EAST=E,VS_WEST=W,VS_ATLANTIC=A,VS_CENTRAL=C,
-         VS_SOUTHEAST=SE,VS_NORTHWEST=NW,VS_PACIFIC=P,VS_SOUTHWEST=SW,
-         PRE_ALLSTAR=PRE,POST_ALLSTAR=POST,MARGIN_LEQ3=`=3`,
-         MARGIN_GEQ10=`=10`) %>%
-  arrange(TEAM_NAME)
+  rename_all(.funs = toupper) %>% 
+  select(-1)
 
-# create win and loss columns:
-x <- names(bbrefStandings[[1]])[-1]
-for (i in 1:length(x)) {
-  bbrefStandings[[1]] <- bbrefStandings[[1]] %>% 
-    separate(x[i],into=paste(x[[i]],c("_W","_L"),sep=""),sep="-",remove=F)
-} ; rm(i)
-
-# win/loss columns per category:
-index <- seq(2,65,3)
-z <- bbrefStandings[[1]][,which(!1:67 %in% index)]
-bbrefStandings[[1]] <- bbrefStandings[[1]][,c(1,index)]
-
-bbrefStandings[[3]] <- z %>% 
-  mutate_at(-1,as.numeric)
-
-# team-vs-team in matrix format:
-y <- as.matrix(bbrefStandings[[2]][,-c(1,2)])
-row.names(y) <- colnames(y)
-bbrefStandings[[2]] <- y
-
-# add names to tables
-names(bbrefStandings) <- c("ExpandedStandings","TeamvsTeam","Expanded_WL")
-
-rm(x,y,z,index)
+# edit names: ----
+names(exp_standings)[5:6] <- paste0("CONF_",names(exp_standings)[5:6])
+names(exp_standings)[7:12] <- paste0("DIV_",names(exp_standings)[7:12])
+names(exp_standings)[13:14] <- paste0(names(exp_standings)[13:14],"_ALLSTAR")
+names(exp_standings)[15:16] <- c("MOV_LEQ3","MOV_GEQ10")
