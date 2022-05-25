33bf4f59e62a37e53f24b67b3c22eeb7efc4f8ea
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-05-25 15:20:27 +0100

    Update minutes-heatmap.R

--- minutes-heatmap.R
+++ minutes-heatmap.R
@@ -1,13 +1,9 @@
 # load libraries: ----
-library(tidyverse)
-library(ggfittext)
-library(hoopR)
-library(showtext)
-font_add_google("Roboto", "Roboto")
-showtext_auto()
+pacman::p_load(tidyverse,hoopR)
 
 # load players box scores: ----
-dfBox <- load_nba_player_box(seasons=2022)
+dfBox <- load_nba_player_box(seasons=2022) %>% 
+  filter(season_type==2) # 3=playoffs
 
 # select team, by abbreviation:
 unique(dfBox$team_abbreviation)
@@ -24,41 +20,28 @@ teamBox <- dfBox %>%
 playerOrder <- teamBox %>% 
   mutate(min=as.numeric(min)) %>% 
   group_by(player) %>%
-  summarise(minPerGame=round(mean(min),1)) %>% 
-  arrange(-minPerGame)
+  summarise(totMin=sum(min),minPerGame=round(mean(min),1),Games=n()) %>% 
+  arrange(desc(totMin)) %>% 
+  select(-2)
 teamBox <- teamBox %>% 
   mutate(player=factor(player, levels = playerOrder$player))
 teamBox$game_number <- teamBox %>% group_indices(game_id)
 
 # filter last N games : ----
+n_games = 20
 teamBoxFilter <- teamBox %>%
-  filter(game_number %in% (unique(teamBox$game_number) %>% tail(10)) & 
-         min !=0) %>% 
+  filter(game_number %in% (unique(teamBox$game_number) %>% tail(n_games)) & 
+           min !=0) %>% 
   mutate(player=factor(player))
 
-# clear environment: ----
-rm(dfBox,playerOrder)
-  
 # create heatmap chart: ----
-p <- teamBoxFilter %>%
+teamBoxFilter %>%
   ggplot(aes(x=as.factor(game_number), fill=as.integer(min), 
              y=player)) +
   geom_tile() +
   scale_y_discrete(limits = rev(levels(teamBoxFilter$player))) +
   scale_fill_gradient(low="#e0efef",high="#006666", guide="colorbar") +
-  geom_text(aes(label = min),fontface="bold",size=6) +
-  theme_minimal() +
+  geom_text(aes(label = min),fontface="bold") +
   labs(title=paste(myTeam,": Minutes distribution",sep=""),
-       subtitle=paste("As of ",Sys.Date(),". Data by {hoopR} | @filippos_pol",sep=""),
-       y="", x="GAME",fill="Minutes") +
-  theme(text = element_text(family="Roboto"),
-        plot.title = element_text(hjust=0.5, size=38,face="bold"),
-        plot.subtitle = element_text(hjust=0.5, size=30),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.title = element_text(size=30),
-        axis.text = element_text(size=25),
-        legend.position='none')
-
-# (optional) save chart as .png file: ----
-ggsave("heatmap.png",p,width=5.5,height=4.5,bg="#ffffff")
+       subtitle=paste("Last ",n_games," games | Source: stats.NBA.com | Plot by@filippos_pol",sep=""),
+       y="", x="Game #",fill="Minutes")
