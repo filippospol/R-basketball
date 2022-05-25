9c853c3de2a6083111e975d337816719c53a9244
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-05-25 13:25:33 +0100

    Update lineup-continuity.R

--- lineup-continuity.R
+++ lineup-continuity.R
@@ -1,9 +1,5 @@
 # load libraries: ----
-library(tidyverse)
-library(showtext)
-library(teamcolors)
-font_add_google("Roboto", "Roboto")
-showtext_auto()
+pacman::p_load(tidyverse,rvest,ggimage)
 
 # get preseason odds and lineup information: ----
 source("https://raw.githubusercontent.com/filippospol/NBA-Data/main/basketball-reference/bbref-starting-lineups.R")
@@ -19,29 +15,25 @@ df <- df %>%
   mutate(DELTA = REAL_WIN_PCT-PROJECTED_WIN_PCT,
          CONTINUITY = round(N_OF_LINEUPS/(W+L),3))
 
-# add team colors:
-df <- df %>% 
-  mutate(COLOR = as.character(league_pal("nba", 1)))
+# add team logos:
+df$TEAM_ID <- hoopR::nba_leaguedashteamstats(season="2021-22") %>%
+  pluck(1) %>% 
+  select(TEAM_ID) %>% 
+  pull()
+df$LOGO <- NA
+for (i in 1:nrow(df)) {
+  df$LOGO[i] <- gsub("XXX",df$TEAM_ID[i],
+                          "https://cdn.nba.com/logos/nba/XXX/global/L/logo.svg")
+}
+rm(list=setdiff(ls(), "df"))
 
 # create chart: ----
-p <- df %>% 
+df %>% 
   ggplot(aes(x=CONTINUITY,y=DELTA,label=TEAM_ABBREVIATION)) +
   scale_y_continuous(labels=scales::percent, limits=c(-0.275,0.275)) +
   geom_hline(yintercept=0,linetype=2,alpha=0.4) +
-  geom_point(pch=21,size=9, fill="#cfedc6") +
-  geom_text(family="Roboto",fontface="bold", size=8) +
+  geom_image(aes(image=LOGO),size=0.07) +
   labs(title=paste('Continuity Chart:',format(Sys.Date(), "%m/%d/%y")),
-       subtitle='Source: Basketball Reference',
-       caption='Plot by: @filippos_pol',
+       subtitle='Source: Basketball Reference | Plot by: @filippos_pol',
        x='# of Distinct Starting Lineups / Games Played',
-       y='Current W% - Projected W%') +
-  theme_minimal() +
-  theme(text=element_text(family = "Roboto"),
-        plot.title = element_text(hjust=0, size=32,face="bold"),
-        plot.subtitle = element_text(hjust=0, size=25, face="italic"),
-        plot.caption = element_text(hjust=0, size=18),
-        axis.title = element_text(size=22),
-        axis.text = element_text(size=20))
-
-# (optional) save chart as .png file: ----
-ggsave("continuity.png",p,width=6.5,height=4,bg="#ffffff")
+       y='Current W% - Projected W%')
