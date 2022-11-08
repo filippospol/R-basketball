65db68b293b10478377561a39b362b4a9463cbab
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-11-08 16:13:32 +0200

    Update bbref-odds.R

--- basketball-reference/bbref-odds.R
+++ basketball-reference/bbref-odds.R
@@ -1,14 +1,13 @@
 # load libraries ----
 pacman::p_load(dplyr,rvest)
 
-# get bbref preseason odds and w-l o/u:
-bbrefOdds <- "https://www.basketball-reference.com/leagues/NBA_2023_preseason_odds.html" %>% 
+bbrefOdds = "https://www.basketball-reference.com/leagues/NBA_2022_preseason_odds.html" %>% 
   read_html() %>% 
   html_elements("table") %>% 
   html_table() %>% 
   pluck(1) %>% 
   select(c(1,4,5)) %>% 
-  mutate(Result = str_sub(Result, start=1, end=5)) %>%
+  mutate(Result = gsub("\\s*\\([^\\)]+\\)","",Result)) %>%
   separate(Result,into=c("W","L"),sep="-",remove=T) %>% 
   mutate(W=as.numeric(W),L=as.numeric(L),
          PROJECTED_WIN_PCT=round((`W-L O/U`+0.5)/82,3),
