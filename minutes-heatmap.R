37ec8dea139edc1c6bca435463c1469595ee67f3
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2022-12-29 11:18:34 +0200

    Update minutes-heatmap.R

--- minutes-heatmap.R
+++ minutes-heatmap.R
@@ -1,24 +1,20 @@
 # load libraries: ----
-pacman::p_load(tidyverse,hoopR)
+pacman::p_load(tidyverse,hoopR,ggtext,sysfonts,showtext)
 
 # load players box scores: ----
-dfBox <- load_nba_player_box(seasons=2022) %>% 
+dfBox <- load_nba_player_box(seasons=2023) %>% 
   filter(season_type==2) # 3=playoffs
 
-# alternative way: ----
-# out <- list()
-# s <- 401442530:401442535
-# for (i in 1:length(s)) {
-#   out[[i]] <- espn_nba_player_box(game_id = s[i]) %>% 
-#     # select(player=athlete_display_name,team_name,team_abbreviation,min) %>% 
-#     mutate(game_id=s[i])
-# }
-# dfBox <- bind_rows(out) ; rm(i,s,out)
-
 # select team, by abbreviation:
-unique(dfBox$team_abbreviation)
-myAbb <- unique(dfBox$team_abbreviation)[23]
-myTeam <- unique(dfBox$team_name)[23]
+ABBS = dfBox$team_abbreviation %>% unique() %>% sort()
+myAbb = ABBS[18]
+myTeam = ABBS[18]
+MyID = nba_leaguedashplayerstats(season="2022-23") %>%
+  pluck(1) %>% 
+  filter(TEAM_ABBREVIATION==myAbb) %>% 
+  pull(TEAM_ID) %>% 
+  unique()
+season=unique(dfBox$season)
 
 # select boxscores for desired team: ----
 teamBox <- dfBox %>% 
@@ -38,22 +34,29 @@ teamBox <- teamBox %>%
 teamBox$game_number <- teamBox %>% group_indices(game_id)
 
 # filter last N games : ----
-n_games = 20
+n_games = max(teamBox$game_number)
 teamBoxFilter <- teamBox %>%
   filter(game_number %in% (unique(teamBox$game_number) %>% tail(n_games)) & 
            min !=0) %>% 
   mutate(player=factor(player))
 
+# clear environment: ----
+rm(dfBox,playerOrder,teamBox,ABBS,MyID,myTeam,n_games)
+
 # create heatmap chart: ----
-season=unique(dfBox$season)
+# change text sizes, fonts etc
 teamBoxFilter %>%
   ggplot(aes(x=as.factor(game_number), fill=as.integer(min), 
              y=player)) +
   geom_tile() +
   scale_y_discrete(limits = rev(levels(teamBoxFilter$player))) +
-  scale_fill_gradient(low="#e0efef",high="#006666", guide="colorbar") +
-  geom_text(aes(label = min),fontface="bold") +
-  labs(title=paste(myTeam,": Minutes distribution",sep=""),
-       subtitle=paste(paste0(season-1,"-",str_sub(season,-2, -1)),
-                      " season | Source: ESPN | Plot by @filippos_pol",sep=""),
-       y="", x="Game #",fill="Minutes")
+  scale_fill_gradient(low="#fff6e5",high="#ffa500", guide="colorbar") +
+  geom_text(aes(label=min)) +
+  labs(title=paste("<img src='",sub("XXX",myAbb,"https://raw.githubusercontent.com/mvpstax/NBA-logos/master/XXX-68x72-6x.png"),
+                   "' width='70'></img>",sep=""),
+       subtitle=paste("Minutes distribution,",paste0(season-1,"-",str_sub(season,-2, -1)),
+                      "| Source: ESPN",sep=" "),
+       caption="Chart by: @filippos_pol",
+       y="", x="Game #",fill="Minutes") +
+  theme(plot.title=element_markdown(face="bold",hjust=0.5),
+        plot.subtitle=element_markdown(color="#777777",hjust=0.5))
