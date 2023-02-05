f49decb46f9d7b174f3f9825bc8e0c52ddfa5811
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2023-02-06 00:10:15 +0200

    Update bbref-position-estimate.R

--- basketball-reference/bbref-position-estimate.R
+++ basketball-reference/bbref-position-estimate.R
@@ -1,56 +1,33 @@
 # load libraries: ----
 pacman::p_load(tidyverse,rvest,janitor)
 
-# get team abbreviations: ----
-y <- "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_National_Basketball_Association/National_Basketball_Association_team_abbreviations" %>% 
+# get position estimates: ----
+season="2019-20"
+
+options(warn=-1)
+df1 = "https://www.basketball-reference.com/leagues/NBA_{paste0(20,substr(season,6,7))}_play-by-play.html" %>% 
+  glue() %>% 
   read_html() %>% 
   html_elements("table") %>% 
   html_table() %>% 
   pluck(1) %>% 
-  row_to_names(row_number = 1) %>% 
-  select(1) %>% pull()
-x <- y
+  row_to_names(row_number=1) %>% 
+  clean_names("all_caps") %>% 
+  group_by(PLAYER) %>% 
+  filter(as.numeric(MP)==max(as.numeric(MP))) %>% 
+  ungroup() %>% 
+  arrange(PLAYER) %>% 
+  select(PLAYER_NAME=PLAYER,PG_PERCENT:C_PERCENT) %>% 
+  mutate_at(-1, ~as.numeric(gsub("%", "", .))/100) %>% 
+  mutate_at(-1, ~ifelse(is.na(.),0,.))
+options(warn=1)
 
-# edit team abbreviations to match with bbref: ----
-x[2] <- "BRK" ; x[4] <- "CHO" ; x[24] <- "PHO"
+df1 = df1 %>% 
+  mutate(POSITION = case_when(
+    PG_PERCENT>=0.85 ~ "POINT",
+    PG_PERCENT+SG_PERCENT>=0.8 ~ "COMBO",
+    PG_PERCENT+SG_PERCENT+SF_PERCENT>=0.85| SG_PERCENT>=0.85 | SF_PERCENT>=0.85 ~ "WING",
+    SF_PERCENT>=0.7 | PF_PERCENT>=0.7 | SF_PERCENT+PF_PERCENT>=0.7~ "FORWARD",
+    TRUE ~ "BIG"
+  )) 
 
-# get position estimates: ----
-out <- list()
-for (i in 1:length(x)) {
-  z <- paste("https://www.basketball-reference.com/teams/",x[i],"/2022.html",
-             sep="") %>% 
-    read_html() %>% 
-    html_nodes(xpath = '//comment()') %>%    
-    html_text() %>%    
-    paste(collapse = '') %>%    
-    read_html() %>% 
-    html_table()
-  
-  target <- 0
-  for (j in 1:length(z)) {
-    if (any(drop_na(z[[j]])=="PG%")) {
-      target <- j
-      break
-    }
-  }
-  
-  df <- z %>% 
-    pluck(target) %>% 
-    row_to_names(1) %>% 
-    clean_names() %>%
-    rename(player_name=x) %>% 
-    rename_all(.funs = toupper) %>% 
-    select(c(2,4:10))
-  
-  df[,4:8] <- data.frame(sapply(df[,4:8], 
-                                function(x) as.numeric(gsub("%", "", x))/100))
-  df[is.na(df)] <- 0
-  df$TEAM_ABBREVIATION <- y[i]
-  df <- df[,c(1,9,2:8)]
-  
-  out[[i]] <- df
-}
-
-# gather all teams' data into one table: ----
-db <- bind_rows(out) %>% arrange(PLAYER_NAME)
-rm(list=setdiff(ls(), "db"))
