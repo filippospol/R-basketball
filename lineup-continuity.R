6b5cb8bbbd3eb85bb51851c8187efea5aee341c9
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2023-03-26 02:16:21 +0200

    updated script

--- lineup-continuity.R
+++ lineup-continuity.R
@@ -1,39 +1,28 @@
-# load libraries: ----
-pacman::p_load(tidyverse,rvest,ggimage)
+pacman::p_load(tidyverse,rvest,ggimage,glue,janitor,hoopR)
 
-# get preseason odds and lineup information: ----
-source("https://raw.githubusercontent.com/filippospol/R-basketball/main/basketball-reference/bbref-starting-lineups.R")
-source("https://raw.githubusercontent.com/filippospol/R-basketball/main/basketball-reference/bbref-odds.R")
+source("https://raw.githubusercontent.com/filippospol/R-basketball/main/scraper%20functions/bbref_lineups.R")
+source("https://raw.githubusercontent.com/filippospol/R-basketball/main/scraper%20functions/bbref_odds.R")
 
-# merge tables into one: ----
-df <- merge(numberOfLineups,bbrefOdds,by="TEAM_NAME") %>%
-  tibble() %>% 
-  mutate_at(-c(1:2,9),as.numeric)
+season = "2022-23"
 
-# create lineup continuity and delta columns:
-df <- df %>% 
-  mutate(DELTA = REAL_WIN_PCT-PROJECTED_WIN_PCT,
-         CONTINUITY = round(N_OF_LINEUPS/(W+L),3))
+df1 = bbref_lineups(season=season)
+df2 = bbref_odds(season=season)
 
-# add team logos:
-df$TEAM_ID <- hoopR::nba_leaguedashteamstats(season="2022-23") %>%
-  pluck(1) %>% 
-  select(TEAM_ID) %>% 
-  pull()
-df$LOGO <- NA
-for (i in 1:nrow(df)) {
-  df$LOGO[i] <- gsub("XXX",df$TEAM_ID[i],
-                          "https://cdn.nba.com/logos/nba/XXX/primary/L/logo.svg")
-}
-rm(list=setdiff(ls(), "df"))
+continuity = left_join(df1,df2,by="TEAM_NAME") %>% 
+  left_join(nba_leaguedashteamstats(season=season) %>% 
+              pluck(1) %>% 
+              select(TEAM_ID,W,L),
+            by="TEAM_ID") ; rm(df1,df2)
 
-# create chart: ----
-df %>% 
-  ggplot(aes(x=CONTINUITY,y=DELTA,label=TEAM_ABBREVIATION)) +
-  scale_y_continuous(labels=scales::percent, limits=c(-0.275,0.275)) +
+continuity %>% 
+  mutate(DELTA=REAL_WINS-PROJECTED_WINS,
+         CONTINUITY=LINEUPS/(as.numeric(W)+as.numeric(L)),
+         LOGO=glue("https://cdn.nba.com/logos/nba/{TEAM_ID}/primary/L/logo.svg")) %>% 
+  ggplot(aes(x=CONTINUITY,y=DELTA,image=LOGO)) +
+  scale_y_continuous(labels=scales::percent, limits=c(-0.25,0.25)) +
   geom_hline(yintercept=0,linetype=2,alpha=0.4) +
-  geom_image(aes(image=LOGO),size=0.07) +
+  geom_image(aes(image=LOGO),size=0.06) +
   labs(title=paste('Continuity Chart:',format(Sys.Date(), "%m/%d/%y")),
-       subtitle='Source: Basketball Reference | Plot by: @filippos_pol',
+       subtitle='Source: Basketball Reference',
        x='# of Distinct Starting Lineups / Games Played',
        y='Current W% - Projected W%')
