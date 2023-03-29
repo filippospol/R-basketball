c962e9d883a6b5b4ee2699839f0bd1c3f164a281
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2023-03-29 18:23:17 +0300

    Update el_boxscores.R

--- scraper functions/el_boxscores.R
+++ scraper functions/el_boxscores.R
@@ -7,7 +7,6 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
 el_boxscores = function(gamecode, seasoncode) {
   pacman::p_load(tidyverse,httr,jsonlite,janitor,glue)
   
@@ -21,25 +20,27 @@ el_boxscores = function(gamecode, seasoncode) {
       pluck("Stats") %>% 
       as_tibble()
     
-    bind_rows(as_tibble(raw[1,]) %>% 
-                pull(PlayersStats) %>% 
-                pluck(1) %>% 
-                as_tibble() %>% 
-                clean_names("all_caps") %>% 
-                mutate(GAMECODE=gamecode,
-                       SEASON=paste0(seasoncode,"-",as.numeric(substr(seasoncode,3,4))+1)),
-              as_tibble(raw[2,]) %>% 
-                pull(PlayersStats) %>% 
-                pluck(1) %>% 
-                as_tibble() %>% 
-                clean_names("all_caps") %>% 
-                mutate(GAMECODE=gamecode,
-                       SEASON=paste0(seasoncode,"-",as.numeric(substr(seasoncode,3,4))+1))) %>%
-      select(GAMECODE,SEASON,IS_STARTER,PLAYER,TEAM,MIN=MINUTES,PTS=POINTS,FG2M=FIELD_GOALS_MADE2,
-             FG2A=FIELD_GOALS_ATTEMPTED2,FG3M=FIELD_GOALS_MADE3,FG3A=FIELD_GOALS_ATTEMPTED3,FTM=FREE_THROWS_MADE,
-             GTA=FREE_THROWS_ATTEMPTED,OREB=OFFENSIVE_REBOUNDS,DREB=DEFENSIVE_REBOUNDS,REB=TOTAL_REBOUNDS,AST=ASSISTANCES,STL=STEALS,
-             TOV=TURNOVERS,BLK=BLOCKS_FAVOUR,BLKD=BLOCKS_AGAINST,PF=FOULS_COMMITED,PFD=FOULS_RECEIVED,PIR=VALUATION) %>% 
-      return()
+    if (!"" %in% raw$Coach) {
+      bind_rows(as_tibble(raw[1,]) %>% 
+                  pull(PlayersStats) %>% 
+                  pluck(1) %>% 
+                  as_tibble() %>% 
+                  clean_names("all_caps") %>% 
+                  mutate(GAMECODE=gamecode,
+                         SEASON=paste0(seasoncode,"-",as.numeric(substr(seasoncode,3,4))+1)),
+                as_tibble(raw[2,]) %>% 
+                  pull(PlayersStats) %>% 
+                  pluck(1) %>% 
+                  as_tibble() %>% 
+                  clean_names("all_caps") %>% 
+                  mutate(GAMECODE=gamecode,
+                         SEASON=paste0(seasoncode,"-",as.numeric(substr(seasoncode,3,4))+1))) %>%
+        select(GAMECODE,SEASON,IS_STARTER,PLAYER,TEAM,MIN=MINUTES,PTS=POINTS,FG2M=FIELD_GOALS_MADE2,
+               FG2A=FIELD_GOALS_ATTEMPTED2,FG3M=FIELD_GOALS_MADE3,FG3A=FIELD_GOALS_ATTEMPTED3,FTM=FREE_THROWS_MADE,
+               GTA=FREE_THROWS_ATTEMPTED,OREB=OFFENSIVE_REBOUNDS,DREB=DEFENSIVE_REBOUNDS,REB=TOTAL_REBOUNDS,AST=ASSISTANCES,STL=STEALS,
+               TOV=TURNOVERS,BLK=BLOCKS_FAVOUR,BLKD=BLOCKS_AGAINST,PF=FOULS_COMMITED,PFD=FOULS_RECEIVED,PIR=VALUATION) %>% 
+        return()
+    }
   }
   
 }
