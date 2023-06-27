715dc8bd7c7f0238732dbae628a470d2cd9ed986
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2023-06-27 23:36:01 +0300

    Update pbp_onoff_ratings.R

--- scraper functions/pbp_onoff_ratings.R
+++ scraper functions/pbp_onoff_ratings.R
@@ -13,18 +13,18 @@ pbp_onoff_ratings = function(season) {
   teams = nba_leaguedashplayerstats(season=season,measure_type="Base") %>% pluck(1) %>% distinct(TEAM_ID) %>% pull()
   
   dfoff = map_df(teams, function(x) {
-    myurl=GET(url = glue("https://api.pbpstats.com/get-on-off/nba/stat?Season={season}&SeasonType=Regular%2BSeason&TeamId={x}&Stat=PtsPer100PossOpponent"))
+    myurl=GET(url = glue("https://api.pbpstats.com/get-on-off/nba/stat?Season={season}&SeasonType=Regular%2BSeason&TeamId={x}&Stat=PtsPer100Poss"))
     
     suppressMessages(
       fromJSON(content(myurl, "text")) %>% pluck(1) %>% tibble() %>% 
         clean_names("all_caps") %>% 
         mutate(TEAM_ID=x) %>% 
-        select(PLAYER_NAME=NAME,TEAM_ID,OFF_RATING_ON=ON,OFF_RATING_OFF=OFF,OFF_RATING_ON_OFF=ON_OFF)
+        select(PLAYER_NAME=NAME,TEAM_ID,MIN=MINUTES_ON,OFF_RATING_ON=ON,OFF_RATING_OFF=OFF,OFF_RATING_ON_OFF=ON_OFF)
     )
   })
   
   dfdef = map_df(teams, function(x) {
-    myurl=GET(url = glue("https://api.pbpstats.com/get-on-off/nba/stat?Season={season}&SeasonType=Regular%2BSeason&TeamId={x}&Stat=PtsPer100Poss"))
+    myurl=GET(url = glue("https://api.pbpstats.com/get-on-off/nba/stat?Season={season}&SeasonType=Regular%2BSeason&TeamId={x}&Stat=PtsPer100PossOpponent"))
     
     suppressMessages(
       fromJSON(content(myurl, "text")) %>% pluck(1) %>% tibble() %>% 
@@ -35,11 +35,12 @@ pbp_onoff_ratings = function(season) {
   })
   
   left_join(dfoff,dfdef, by=c("PLAYER_NAME","TEAM_ID")) %>% 
-    mutate_at(-c(1,2), ~round(as.numeric(.),2)) %>% 
+    mutate_at(-c(1:3), ~ round(as.numeric(.),2)) %>% 
     mutate(NET_RATING_ON=OFF_RATING_ON-DEF_RATING_ON,NET_RATING_OFF=OFF_RATING_OFF-DEF_RATING_OFF,
-           NET_RATING_ON_OFF=OFF_RATING_ON_OFF-DEF_RATING_ON_OFF) %>% 
-    left_join(nba_leaguedashplayerstats(season=season) %>% pluck(1) %>% select(PLAYER_NAME,PLAYER_ID,TEAM_ID,TEAM_ABBREVIATION),
-              by=c("PLAYER_NAME","TEAM_ID")) %>% 
-    select(1,12,2,13,3:11) %>% 
+           NET_RATING_ON_OFF=OFF_RATING_ON_OFF-DEF_RATING_ON_OFF) %>%
+    group_by(PLAYER_NAME) %>% 
+    summarise_at(vars(3:11), ~ sum(MIN*.)/sum(MIN)) %>%
+    left_join(nba_leaguedashplayerstats(season=season) %>% pluck(1) %>% select(PLAYER_NAME,PLAYER_ID,TEAM_ID,TEAM_ABBREVIATION)
+              ,.,by="PLAYER_NAME") %>% 
     return()
 }
