1cd47fa3c3af69316b64bb825e1e2c9183af54e0
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2023-08-20 21:02:06 +0300

    function that converts a {gt} table into a {ggplot} object

---
+++ as_ggplot.R
@@ -0,0 +1,16 @@
+pacman::p_load(ggplot2,gt,gtsummary,magick)
+as_ggplot <- function(x, ...) {
+  
+  if (!inherits(x, c("gt_tbl", "gtsummary"))) stop("`x=` must be a 'gt' or 'gtsummary' table", call. = FALSE)
+  
+  if (inherits(x, "gtsummary")) x <- gtsummary::as_gt(x)
+  
+  path_gt_table_image <- fs::file_temp(ext = "png")
+  gt_table_image <- gt::gtsave(x, filename = path_gt_table_image, dpi=400, ...)
+  
+  table_img <-
+    magick::image_read(path_gt_table_image) %>%
+    magick::image_ggplot(interpolate = TRUE)
+  
+  table_img
+}
