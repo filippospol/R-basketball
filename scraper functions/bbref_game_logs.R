6e9513a16071cd9e85f7ebee59a3da620af64b80
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-01-03 15:31:53 +0200

    .

--- scraper functions/bbref_game_logs.R
+++ scraper functions/bbref_game_logs.R
@@ -1,54 +1,106 @@
-pacman::p_load(tidyverse,rvest,janitor,glue)
+pacman::p_load(tidyverse,rvest,glue,janitor,lubridate)
 
-# Enter season:
-season = 2025
-
-# Get players ID codes from the Per Game stats table:
-playersID = "https://www.basketball-reference.com/leagues/NBA_{season}_advanced.html" %>% 
-  glue() %>% 
-  read_html() %>% 
-  html_nodes("table") %>% 
-  html_nodes("a") %>% 
-  html_attr("href") %>% 
-  str_subset(pattern="/players/") %>% 
-  unique() %>% 
-  sort() %>% 
-  gsub(".html","",.)
-
-gamelogs = list() ; k=0
-for (i in 1:25) {             # change 25 to length(playersID)
-  
-  # control scraper traffic on Basketball Reference (max calls per minute = 20)
-  k = k+1
-  if (k %in% c(seq(20,660,20) + 1)) Sys.sleep(60)
-  
-  # URL of game logs:
-  playerURL = "https://www.basketball-reference.com{playersID[i]}/gamelog/{season}" %>% 
+bbref_game_log = function(gameID) {
+  # HTML that contains the boxscore:
+  baseHTML = "https://www.basketball-reference.com/boxscores/{gameID}.html" %>% 
     glue() %>% 
     read_html()
   
-  # Extract player name:
-  playerName = playerURL %>% 
+  # matchup info:
+  matchup_date = baseHTML %>% 
     html_nodes("h1") %>% 
     html_text() %>% 
-    substring(4) %>% 
-    gsub(paste0(as.numeric(season)-1,"-",substring(season,3)," Game Log\n\t\t\t"),"",.) %>% 
-    str_trim()
+    strsplit(split=" Box Score, ")
   
-  # Get game logs:
-  gamelogs[[i]] = playerURL %>% 
-    html_elements("table") %>% 
-    html_table() %>% 
-    pluck(8) %>% 
-    clean_names("all_caps") %>% 
-    filter(DATE !="Date") %>% 
-    mutate(PLAYER_NAME=playerName,PLAYER_BBREF_ID=gsub("/players/","",playersID[i]),
-           SEASON=paste0(as.numeric(season)-1,"-",substring(season,3)), .before=1)
+  # matchup:
+  matchup = matchup_date %>%
+    pluck(1) %>%
+    pluck(1) %>% 
+    gsub("at","@",.)
   
-  print(i)
+  # date:
+  date = matchup_date %>%
+    pluck(1) %>%
+    pluck(2) %>% 
+    mdy()
+  rm(matchup_date)
   
+  # Inactive players full info:
+  rawInactive = baseHTML %>% 
+    html_nodes('div:not([id]):not([class])') %>% 
+    html_text() %>% 
+    str_subset("Inactive") %>% 
+    pluck(2) %>% 
+    str_replace(.,"Inactive","") %>% 
+    strsplit(., ':', fixed = TRUE) %>% 
+    pluck(1) %>% 
+    tail(1) %>% 
+    str_split(.,"\\s{2,}") %>% 
+    pluck(1) %>% 
+    head(2)
+  
+  # Team acronym and inactive players:
+  awayTeam = rawInactive[[1]] %>% 
+    str_trim() %>% 
+    substr(1,3)
+  awayInactive = rawInactive[[1]] %>% 
+    str_trim() %>% 
+    str_sub(5) %>% 
+    strsplit(",") %>% 
+    lapply(str_trim) %>% 
+    pluck(1)
+  
+  homeTeam = rawInactive[[2]] %>% 
+    str_trim() %>% 
+    substr(1,3)
+  homeInactive = rawInactive[[2]] %>% 
+    str_trim() %>% 
+    str_sub(5) %>% 
+    strsplit(",") %>% 
+    lapply(str_trim) %>% 
+    pluck(1)
+  
+  # Get all tables from the page:
+  gameTables = baseHTML %>% 
+    html_nodes("table") %>% 
+    html_table()
+  
+  # Get away and home boxscores and add the additional info:
+  rawBox = bind_rows(
+    gameTables %>% 
+      pluck(1) %>% 
+      row_to_names(1) %>% 
+      clean_names("all_caps") %>% 
+      filter(STARTERS != "Reserves") %>% 
+      rename(PLAYER_NAME=STARTERS) %>% 
+      add_row(PLAYER_NAME=awayInactive,.before=dim(.)[1]) %>% 
+      mutate(across(everything(), ~replace_na(.x, "Inactive"))) %>%
+      mutate(IS_STARTER=c(rep(T,5),rep(F,dim(.)[1]-5)),.before=MP) %>%
+      mutate(GAME_ID=gameID,MATCHUP=matchup,DATE=date,TEAM=awayTeam,
+             .before=1),
+    gameTables %>% 
+      pluck(9) %>% 
+      row_to_names(1) %>% 
+      clean_names("all_caps") %>% 
+      filter(STARTERS != "Reserves") %>% 
+      rename(PLAYER_NAME=STARTERS) %>% 
+      add_row(PLAYER_NAME=homeInactive,.before=dim(.)[1]) %>% 
+      mutate(across(everything(), ~replace_na(.x, "Inactive"))) %>%
+      mutate(IS_STARTER=c(rep(T,5),rep(F,dim(.)[1]-5)),.before=MP) %>%
+      mutate(GAME_ID=gameID,MATCHUP=matchup,DATE=date,TEAM=homeTeam,
+             .before=1)
+  ) ; rm(list=setdiff(ls(),"rawBox"))
+  
+  # Clean data and print table:
+  rawBox %>% 
+    filter(PLAYER_NAME != "Team Totals") %>% 
+    mutate(IS_STARTER=ifelse(grepl("[0-9]",MP),IS_STARTER,NA)) %>% 
+    mutate(STATUS=ifelse(grepl("[0-9]",MP),"Active",MP),.before=MP) %>% 
+    mutate(across(MP:X,~ifelse(grepl("[0-9]",MP),.,NA))) %>% 
+    rename(PLUS_MINUS=X) %>% 
+    separate(MP,c("M","S"),sep=":") %>% 
+    mutate(MP=as.numeric(M)+(as.numeric(S)/60),.after=STATUS) %>% 
+    select(-c(M,S)) %>% 
+    mutate_at(9:28, as.numeric) %>% 
+    return()
 }
-
-# rm(playerURL,playerName,i,k,season)
-
-gamelogsAll = bind_rows(gamelogs)
