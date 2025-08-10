f44bb8dc9c3a6b081a17f895c7697b3915223ae5
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-08-11 00:17:37 +0300

    Updated.

--- scraper functions/bbref_gameid_date.R
+++ scraper functions/bbref_gameid_date.R
@@ -1,3 +1,14 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# This script scrapes game ids for a given date from Basketball Reference.
+# Season format: yyyy.
+# Use these ids to get bbref game logs to inspect games missed for a given player.
+# Author: Filippos Polyzos
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 pacman::p_load(tidyverse,rvest,glue,janitor,lubridate)
 
 bbref_gameid_date = function(season,mm,dd) {
@@ -11,5 +22,6 @@ bbref_gameid_date = function(season,mm,dd) {
     str_subset("/boxscores/20") %>% 
     gsub("/boxscores/","",.) %>% 
     gsub(".html","",.) %>% 
+    unique() %>% 
     return()
 }
