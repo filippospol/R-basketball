48d940c0607522c078fa3ec66f8b0616198b04a3
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-08-11 01:25:00 +0300

    Updated.

--- scraper functions/bbref_shooting.R
+++ scraper functions/bbref_shooting.R
@@ -1,7 +1,7 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-# This script scrapes shooting stats from Basketball Reference
+# This script scrapes shooting stats from Basketball Reference.
 # Author: Filippos Polyzos
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -19,15 +19,24 @@ bbref_shooting = function(season) {
       pluck(1) %>% 
       row_to_names(row_number=1) %>% 
       clean_names("all_caps") %>% 
+      mutate(TEAM = case_when(
+        TEAM=="BRK" ~ "BKN",
+        TEAM=="CHO" ~ "CHA",
+        TEAM=="PHO" ~ "PHX",
+        TRUE ~ TEAM
+      )) %>% 
       group_by(PLAYER) %>% 
-      filter(as.numeric(MP)==max(as.numeric(MP))) %>%  
+      mutate(TEAM=gsub("2TM|3TM|4TM","",paste0(TEAM,collapse="/")),
+             TEAM=gsub("^/","",TEAM),
+             SEASON=season) %>% 
+      filter(as.numeric(MP)==max(as.numeric(MP))) %>%
       ungroup() %>% 
       arrange(PLAYER)
   ) %>% 
-    select(PLAYER_NAME=PLAYER,AVG_FG_DIST=DIST,FREQ_FG2=X2P,FREQ_0_3=X0_3,FREQ_3_10=X3_10,
+    select(SEASON,PLAYER_NAME=PLAYER,TEAM,AVG_FG_DIST=DIST,FREQ_FG2=X2P,FREQ_0_3=X0_3,FREQ_3_10=X3_10,
            FREQ_10_16=X10_16,FREQ_16_FG3=X16_3P,FREQ_FG3=X3P,EFF_FG2=X2P_2,EFF_0_3=X0_3_2,
            EFF_3_10=X3_10_2,EFF_10_16=X10_16_2,EFF_16_FG3=X16_3P_2,ASSISTED_FG2=X2P_3,
            ASSISTED_FG3=X3P_3,FREQ_DUNKS=PERCENT_FGA,DUNKS=NUMBER,FREQ_CORNER_FG3A=PERCENT_3PA,
-           CORNER_FG3_PCT=NUMBER_2,HEAVES_FGA=ATT,HEAVES_FGM=NUMBER_2) %>% 
+           CORNER_FG3_PCT=X3P_PERCENT,HEAVES_FGA=ATT,HEAVES_FGM=MD) %>% 
     return()
 }
