1ea4fd605fe5d04f1fe665db521e8c2ea1747f6b
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-08-11 01:30:25 +0300

    Updated.

--- scraper functions/bbref_play_by_play.R
+++ scraper functions/bbref_play_by_play.R
@@ -1,7 +1,7 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-# This script scrapes play-by-play stats from Basketball Reference
+# This script scrapes play-by-play stats from Basketball Reference.
 # Author: Filippos Polyzos
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -19,12 +19,21 @@ bbref_play_by_play = function(season) {
       pluck(1) %>% 
       row_to_names(row_number=1) %>% 
       clean_names("all_caps") %>% 
+      mutate(TEAM = case_when(
+        TEAM=="BRK" ~ "BKN",
+        TEAM=="CHO" ~ "CHA",
+        TEAM=="PHO" ~ "PHX",
+        TRUE ~ TEAM
+      )) %>% 
       group_by(PLAYER) %>% 
-      filter(as.numeric(MP)==max(as.numeric(MP))) %>%  
+      mutate(TEAM=gsub("2TM|3TM|4TM","",paste0(TEAM,collapse="/")),
+             TEAM=gsub("^/","",TEAM)) %>% 
+      filter(as.numeric(MP)==max(as.numeric(MP))) %>%
       ungroup() %>% 
-      arrange(PLAYER)
+      arrange(PLAYER) %>% 
+      mutate(SEASON=season)
   ) %>% 
-    select(PLAYER_NAME=PLAYER,TOV_BAD_PASS=BAD_PASS,TOV_LOST_BALL=LOST_BALL,SHOOT_PF=SHOOT,OFF_PF=OFF,
-           SHOOT_PFD=SHOOT_2,OFF_PFD=OFF_2,PGA,AND1,BLKD_FGA=BLKD) %>% 
+    select(SEASON,PLAYER_NAME=PLAYER,TOV_BAD_PASS=BAD_PASS,TOV_LOST_BALL=LOST_BALL,SHOOT_PF=SHOOT,OFF_PF=OFF,
+           SHOOT_PFD=SHOOT_2,OFF_PFD=OFF_2,AST_PTS=PGA,AND1,BLKD_FGA=BLKD) %>% 
     return()
 }
