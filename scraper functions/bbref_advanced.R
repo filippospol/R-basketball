4a51bf29528008c95e66a9123b6e10faf60ae793
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-08-11 00:11:14 +0300

    Updated.

--- scraper functions/bbref_advanced.R
+++ scraper functions/bbref_advanced.R
@@ -1,7 +1,7 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-# This script scrapes advanced stats from Basketball Reference
+# This script scrapes advanced stats from Basketball Reference.
 # Author: Filippos Polyzos
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -11,21 +11,28 @@ bbref_advanced = function(season) {
   pacman::p_load(tidyverse,rvest,janitor,glue)
   
   suppressWarnings(
-  "https://www.basketball-reference.com/leagues/NBA_{paste0(20,substr(season,6,7))}_advanced.html" %>% 
-    glue() %>% 
-    read_html() %>% 
-    html_elements("table") %>% 
-    html_table() %>% 
-    pluck(1) %>% 
-    clean_names("all_caps") %>% 
-    group_by(PLAYER) %>% 
-    mutate(TEAM=gsub("TOT/","",paste0(TEAM,collapse="/")),
-           TEAM=str_sub(TEAM,5)) %>% 
-    filter(as.numeric(MP)==max(as.numeric(MP))) %>%
-    ungroup() %>% 
-    arrange(PLAYER)
-) %>% 
-    select(PLAYER_NAME=PLAYER,TEAM,POS,AGE,G,MP,PER,TS_PCT=TS_PERCENT,FG3_RATE=X3P_AR,FTA_RATE=F_TR,OREB_PCT=ORB_PERCENT,DREB_PCT=DRB_PERCENT,
+    "https://www.basketball-reference.com/leagues/NBA_{paste0(20,substr(season,6,7))}_advanced.html" %>% 
+      glue() %>% 
+      read_html() %>% 
+      html_elements("table") %>% 
+      html_table() %>% 
+      pluck(1) %>% 
+      clean_names("all_caps") %>% 
+      mutate(TEAM = case_when(
+        TEAM=="BRK" ~ "BKN",
+        TEAM=="CHO" ~ "CHA",
+        TEAM=="PHO" ~ "PHX",
+        TRUE ~ TEAM
+      )) %>% 
+      group_by(PLAYER) %>% 
+      mutate(TEAM=gsub("2TM|3TM","",paste0(TEAM,collapse="/")),
+             TEAM=gsub("^/","",TEAM)) %>% 
+      filter(as.numeric(MP)==max(as.numeric(MP))) %>%
+      ungroup() %>% 
+      arrange(PLAYER)
+  ) %>% 
+    mutate(SEASON=season) %>% 
+    select(SEASON,PLAYER_NAME=PLAYER,TEAM,POS,AGE,GP=G,MIN=MP,PER,TS_PCT=TS_PERCENT,FG3_RATE=X3P_AR,FTA_RATE=F_TR,OREB_PCT=ORB_PERCENT,DREB_PCT=DRB_PERCENT,
            AST_PCT=AST_PERCENT,STL_PCT=STL_PERCENT,BLK_PCT=BLK_PERCENT,TOV_PCT=TOV_PERCENT,USG_PCT=USG_PERCENT,PER,OWS:WS_48,OBPM:VORP) %>% 
     return()
 }
