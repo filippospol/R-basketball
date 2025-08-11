1be3f4bdb42c0fe5db424e96dc20c6e276c4991e
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2025-08-11 15:31:22 +0300

    Updated.

--- scraper functions/pbp_teams_shot_distribution.R
+++ scraper functions/pbp_teams_shot_distribution.R
@@ -27,8 +27,9 @@ pbp_teams_shot_distribution = function(season,season_type="Regular Season",per_m
       mutate(MID_RANGE_FGM=as.numeric(SHORT_MID_RANGE_FGM+LONG_MID_RANGE_FGM),
              MID_RANGE_FGA=as.numeric(SHORT_MID_RANGE_FGA+LONG_MID_RANGE_FGA),
              MID_RANGE_FREQUENCY=MID_RANGE_FGA/(as.numeric(FG2A+FG3A)),
-             MID_RANGE_ACCURACY=MID_RANGE_FGM/MID_RANGE_FGA %>% round(3)) %>% 
-      select(TEAM_ID,TEAM_ABBREVIATION,RIM_FREQ=AT_RIM_FREQUENCY,RIM_EFF=AT_RIM_ACCURACY,
+             MID_RANGE_ACCURACY=MID_RANGE_FGM/MID_RANGE_FGA %>% round(3),
+             SEASON=season) %>% 
+      select(SEASON,TEAM_ID,TEAM_ABBREVIATION,RIM_FREQ=AT_RIM_FREQUENCY,RIM_EFF=AT_RIM_ACCURACY,
              SHORT_MID_FREQ=SHORT_MID_RANGE_FREQUENCY,SHORT_MID_EFF=SHORT_MID_RANGE_ACCURACY,
              LONG_MID_FREQ=LONG_MID_RANGE_FREQUENCY,LONG_MID_EFF=LONG_MID_RANGE_ACCURACY,
              ALL_MID_FREQ=MID_RANGE_FREQUENCY,ALL_MID_EFF=MID_RANGE_ACCURACY,
@@ -36,6 +37,6 @@ pbp_teams_shot_distribution = function(season,season_type="Regular Season",per_m
              ARC_FREQ=ARC3FREQUENCY,ARC_EFF=ARC3ACCURACY,SECONDS_PER_POSS_OFF,SECONDS_PER_POSS_DEF),
     by="TEAM_ID"
   ) %>% 
-    mutate_at(-c(1:3), as.numeric) %>% arrange(TEAM_ABBREVIATION) %>% 
+    mutate_at(-c(1:4), as.numeric) %>% arrange(TEAM_ABBREVIATION) %>% 
     return()
 }
